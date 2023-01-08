fe884eaf0afeba777053eda96c64ab4119cce853
Gerry1Laxy <mr.john.milton@yandex.ru>
2023-01-08 15:48:08 +0300

    added sixth hw

---
+++ django_06/logistic/views.py
@@ -0,0 +1,23 @@
+from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filterset_fields = ['title', 'description']
+    # при необходимости добавьте параметры фильтрации
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['products']
+    search_fields = ['products__id']
+    # при необходимости добавьте параметры фильтрации

f77026291dc7083a8992b39c43dca3334f06e4b4
Gerry1Laxy <mr.john.milton@yandex.ru>
2023-02-21 21:10:31 +0300

    completed permisstion hw

---
+++ django_07/api_with_restrictions/advertisements/serializers.py
@@ -0,0 +1,53 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from advertisements.models import Advertisement
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer для пользователя."""
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name',
+                  'last_name',)
+
+
+class AdvertisementSerializer(serializers.ModelSerializer):
+    """Serializer для объявления."""
+
+    creator = UserSerializer(
+        read_only=True,
+    )
+
+    class Meta:
+        model = Advertisement
+        fields = ('id', 'title', 'description', 'creator',
+                  'status', 'created_at', )
+
+    def create(self, validated_data):
+        """Метод для создания"""
+
+        # Простановка значения поля создатель по-умолчанию.
+        # Текущий пользователь является создателем объявления
+        # изменить или переопределить его через API нельзя.
+        # обратите внимание на `context` – он выставляется автоматически
+        # через методы ViewSet.
+        # само поле при этом объявляется как `read_only=True`
+        validated_data["creator"] = self.context["request"].user
+        return super().create(validated_data)
+
+    def validate(self, data):
+        """Метод для валидации. Вызывается при создании и обновлении."""
+
+        # TODO: добавьте требуемую валидацию
+        user_id = self.context['request'].user.id
+        count_opened = Advertisement.objects.filter(
+            creator=user_id, status='OPEN'
+        ).count()
+        if count_opened <= 11:
+            return data
+        else:
+            raise serializers.ValidationError(
+                'More than 10 opened advertisement'
+            )
